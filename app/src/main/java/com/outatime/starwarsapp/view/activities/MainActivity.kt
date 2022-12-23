1e24dcdef66137ec58d436bceed01a9a96506334
danieltorres4 <dst990502@outlook.com>
2022-12-23 15:11:48 -0600

    Adding new fonts and a new drawable resource file (background_sw)

--- app/src/main/java/com/outatime/starwarsapp/view/activities/MainActivity.kt
+++ app/src/main/java/com/outatime/starwarsapp/view/activities/MainActivity.kt
@@ -3,10 +3,14 @@ package com.outatime.starwarsapp.view.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.outatime.starwarsapp.R
+import com.outatime.starwarsapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
