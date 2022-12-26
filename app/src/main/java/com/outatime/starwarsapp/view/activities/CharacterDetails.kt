783cbec3348ef7ff930068673cd1f8f98eb96a06
danieltorres4 <dst990502@outlook.com>
2022-12-26 14:06:24 -0600

    Implementing changes from popup branch

--- app/src/main/java/com/outatime/starwarsapp/view/activities/CharacterDetails.kt
+++ app/src/main/java/com/outatime/starwarsapp/view/activities/CharacterDetails.kt
@@ -4,8 +4,11 @@ import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.ContextMenu
+import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.outatime.starwarsapp.R
 import com.outatime.starwarsapp.databinding.ActivityCharacterDetailsBinding
 import com.outatime.starwarsapp.model.*
@@ -20,6 +23,12 @@ import retrofit2.Response
 class CharacterDetails : AppCompatActivity() {
     private lateinit var binding: ActivityCharacterDetailsBinding
     private lateinit var mp: MediaPlayer
+    private lateinit var planetDiameter: String
+    private lateinit var planetClimate: String
+    private lateinit var planetOrbitalPeriod: String
+    private lateinit var planetRotationPeriod: String
+    private lateinit var planetPopulation: String
+    private lateinit var planetTerrain: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityCharacterDetailsBinding.inflate(layoutInflater)
@@ -86,7 +95,14 @@ class CharacterDetails : AppCompatActivity() {
                     //Log.d(Constants.LOGTAG, "Data: ${response.body().toString()}")
 
                     with(binding) {
+                        registerForContextMenu(binding.tvPlanetDetails)
                         tvPlanetDetails.text = getString(R.string.tv_result_planet_name, response.body()?.name)
+                        planetClimate = response.body()?.climate.toString()
+                        planetDiameter = response.body()?.diameter.toString()
+                        planetOrbitalPeriod = response.body()?.orbital_period.toString()
+                        planetPopulation = response.body()?.population.toString()
+                        planetRotationPeriod = response.body()?.rotation_period.toString()
+                        planetTerrain = response.body()?.terrain.toString()
                     }
                 }
 
@@ -98,6 +114,45 @@ class CharacterDetails : AppCompatActivity() {
         }
     }
 
+    override fun onCreateContextMenu(
+        menu: ContextMenu?,
+        v: View?,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        menuInflater.inflate(R.menu.menu_planet_item, menu)
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.item_1 -> {
+                //item.setTitle(planetClimate)
+                Toast.makeText(this@CharacterDetails, planetClimate, Toast.LENGTH_SHORT).show()
+            }
+
+            R.id.item_2 -> {
+                Toast.makeText(this@CharacterDetails, planetDiameter, Toast.LENGTH_SHORT).show()
+            }
+
+            R.id.item_3 -> {
+                Toast.makeText(this@CharacterDetails, planetOrbitalPeriod, Toast.LENGTH_SHORT).show()
+            }
+
+            R.id.item_4 -> {
+                Toast.makeText(this@CharacterDetails, planetPopulation, Toast.LENGTH_SHORT).show()
+            }
+
+            R.id.item_5 -> {
+                Toast.makeText(this@CharacterDetails, planetRotationPeriod, Toast.LENGTH_SHORT).show()
+            }
+
+            R.id.item_6 -> {
+                Toast.makeText(this@CharacterDetails, planetTerrain, Toast.LENGTH_SHORT).show()
+            }
+        }
+        return super.onContextItemSelected(item)
+    }
+
     override fun onPause() {
         super.onPause()
         mp.pause()
@@ -106,5 +161,17 @@ class CharacterDetails : AppCompatActivity() {
     override fun onRestart() {
         super.onRestart()
         mp.start()
+    }
+
+    fun onClick(view: View) {
+        val ad = AlertDialog.Builder(this@CharacterDetails)
+        ad.setTitle(R.string.alert_dialog_title)
+        ad.setMessage(R.string.alert_dialog_msg)
+
+        ad.setPositiveButton(R.string.alert_dialog_positive_response) {dialog, which ->
+
+        }
+
+        ad.show()
     }
 }
